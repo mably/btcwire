fb362608ce45ad7b77ee397f3cdfba78cd6e5fbc
mably <mably>
2014-11-18 12:30:42 +0100

    MsgTx test fix, time is supposed to be in seconds
    Conflicts:
    	msgtx.go

--- msgtx.go
+++ msgtx.go
@@ -438,7 +438,8 @@ func (msg *MsgTx) MaxPayloadLength(pver uint32) uint32 {
 func NewMsgTx() *MsgTx {
 	return &MsgTx{
 		Version: TxVersion,
-		Time:    time.Unix(int64(0), 0),
+		//Time:    time.Unix(int64(0), 0),
+		Time:    time.Unix(time.Now().Unix(), 0), // ppc: TODO(kac-) Now() better than zero?
 		TxIn:    make([]*TxIn, 0, defaultTxInOutAlloc),
 		TxOut:   make([]*TxOut, 0, defaultTxInOutAlloc),
 	}
